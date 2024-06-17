efa811ad3be63528acb126c4b016fc47aa973aff
yanabarak <yanabarak94@gmail.com>
2024-06-17 14:43:52 +0300

    dropdown submenu

--- src/components/SubMenu.vue
+++ src/components/SubMenu.vue
@@ -1,25 +1,74 @@
 <template>
-  <div class="nav-item dropdown">
+  <div class="nav-item dropdown" :class="{ show: showDropdown }">
     <a
       class="nav-link dropdown-toggle d-flex align-items-center py-0 pe-0 fw-bold"
       href="#"
       id="navbarDropdownMenuLink"
-      data-toggle="dropdown"
+      @click="toggleDropdown"
       aria-haspopup="true"
-      aria-expanded="false"
+      :aria-expanded="showDropdown"
     >
       <RoundedImage style="width: 34px; height: 34px" />
       <span class="ms-3 me-1">Josh Bloch</span>
     </a>
-    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+    <ul
+      class="dropdown-menu"
+      :class="{ show: showDropdown }"
+      aria-labelledby="navbarDropdownMenuLink"
+    >
       <li><a class="dropdown-item" href="#">Action</a></li>
       <li><a class="dropdown-item" href="#">Another action</a></li>
+      <li class="nav-item dropdown" :class="{ show: showSubmenu }">
+        <a
+          class="dropdown-item"
+          @click="toggleSubmenu"
+          href="#"
+          aria-haspopup="true"
+          :aria-expanded="showSubmenu"
+        >
+          Language
+        </a>
+        <ul
+          class="dropdown-menu"
+          :class="{ show: showSubmenu }"
+          aria-labelledby="navbarDropdownMenuLink"
+        >
+          <li><a class="dropdown-item" href="#">EN</a></li>
+          <li><a class="dropdown-item" href="#">EN</a></li>
+        </ul>
+      </li>
     </ul>
   </div>
 </template>
+
 <script setup>
+import { ref } from 'vue';
 import RoundedImage from './RoundedImage.vue';
+
+const showDropdown = ref(false);
+const showSubmenu = ref(false);
+const showSubsubmenu1 = ref(false);
+
+const toggleDropdown = () => {
+  showDropdown.value = !showDropdown.value;
+  if (showDropdown.value) {
+    showSubmenu.value = false;
+  }
+};
+
+const toggleSubmenu = () => {
+  showSubmenu.value = !showSubmenu.value;
+  if (showSubmenu.value) {
+    showDropdown.value = true;
+  }
+};
+
+const toggleSubsubmenu1 = event => {
+  event.preventDefault();
+  showSubsubmenu1.value = !showSubsubmenu1.value;
+};
 </script>
+
 <style scoped>
 .dropdown:before {
   content: '';
@@ -34,5 +83,10 @@ import RoundedImage from './RoundedImage.vue';
 }
 .nav-link {
   color: #000 !important;
+}
+.dropdown-menu .dropdown-menu {
+  top: 0px;
+  left: auto;
+  right: 99% !important;
 }
 </style>
